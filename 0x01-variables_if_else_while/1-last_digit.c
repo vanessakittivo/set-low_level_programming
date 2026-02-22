9226664b7593d87078e93b114827593443065bd3
vanessa <wambuinessie@gmail.com>
2026-02-22 17:01:00 +0000

    Task 1: Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,11 +5,12 @@
  * main - Prints the last digit of a randomly generated number
  *
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	int n;
 	int last_digit;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
