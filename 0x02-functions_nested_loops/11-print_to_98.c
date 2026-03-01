8847cc38cc68fb9d76a990bfd07e94e2bd739676
vanessa <wambuinessie@gmail.com>
2026-03-01 17:49:22 +0000

    Complete 0x02 functions nested loops tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * print_number - helper to print any integer (handles negative & multi-digit)
+ * @n: number to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	print_number(n / 10);
+
+	_putchar('0' + (n % 10));
+	}
+
+/**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: starting number
  */
@@ -10,46 +28,27 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
-	{
-		if (i != n)
-	{
-		_putchar(',');
-		_putchar(' ');
-	}
-	if (i >= 0 && i <= 9)
-	{
-		_putchar('0' + i);
-	}
-	else if (i >= 10 && i <= 99)
-	{
-		_putchar('0' + (i / 10));
-		_putchar('0' + (i % 10));
-	}
-	else if (i < 0 && i >= -9)
-	{
-		_putchar('-');
-		_putchar('0' + (-i));
-	}
-	else if (i < 0 && i >= -99)
-	{
-		_putchar('-');
-		_putchar('0' + ((-i) / 10));
-		_putchar('0' + ((-i) % 10));
-	}
-}
-	}
-	else
-	{
-	for (i = n; i >= 98; i--)
+	for (i = n; i <= 98; i++)
 	{
 	if (i != n)
 	{
 		_putchar(',');
 		_putchar(' ');
 	}
-		_putchar('0' + (i / 10));
-		_putchar('0' + (i % 10));
+		print_number(i);
+	}
+	}
+	else
+	{
+	for (i = n; i >= 98; i--)
+	{
+
+	if (i != n)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+		print_number(i);
 	}
 	}
 		_putchar('\n');
