728e67382674e1a348cea7df6225954b657107a2
vanessa <wambuinessie@gmail.com>
2026-04-01 21:32:42 +0000

    Complete 0x0A argc argv tasks

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed
- * @argc: argument count
- * @argv: argument vector
+ * main - prints the number of arguments passed to it
+ * @argc: number of command line arguments
+ * @argv: array containing command line arguments
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
 
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
