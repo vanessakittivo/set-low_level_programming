064f862497f55757788d7374f14f972eaf97c21d
vanessakittivo <wambuinessie@gmail.com>
2026-05-10 17:26:37 +0300

    Add singly linked lists project

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,9 +12,9 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 
 	while (h != NULL)
-{
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
+	{
+		if (h->str == NULL)
+		 printf("[0] (nil)\n");
 	else
 	printf("[%u] %s\n", h->len, h->str);
 
