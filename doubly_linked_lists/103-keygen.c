2d0cf16df88b3c0e22a2d1273feb781201e386ca
vanessakittivo <wambuinessie@gmail.com>
2026-05-30 02:57:20 +0300

    Add crackme5 keygen

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -2,87 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 
+static const char *charset =
+	"A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
 /**
- * keygen5 - generates a valid key for crackme5 given a username
- * Usage: ./keygen5 username
+ * get_indices - compute key character indices for positions 0-2
+ * @username: the username string
+ * @n: length of username
+ * @idx: output array of at least 3 ints
  */
-
-static const char charset[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-
-static int f1(int n)
+static void get_indices(const char *username, int n, int *idx)
 {
-	return (n ^ 0x3b) & 0x3f;
-}
-
-static int f2(const char *s, int n)
-{
-	int sum = 0, i;
+	int sum = 0, prod = 1, i;
 
 	for (i = 0; i < n; i++)
-		sum += (int)s[i];
-	return (sum ^ 0x4f) & 0x3f;
+	{
+		sum += (int)username[i];
+		prod *= (int)username[i];
+	}
+	idx[0] = (n ^ 0x3b) & 0x3f;
+	idx[1] = (sum ^ 0x4f) & 0x3f;
+	idx[2] = (prod ^ 0x55) & 0x3f;
 }
 
-static int f3(const char *s, int n)
+/**
+ * get_rand_indices - compute key indices for positions 3-5 using rand
+ * @username: the username string
+ * @n: length of username
+ * @idx: output array of at least 3 ints
+ */
+static void get_rand_indices(const char *username, int n, int *idx)
 {
-	int prod = 1, i;
+	int max_c, sq_sum, i, r;
 
+	max_c = (int)username[0];
+	sq_sum = 0;
 	for (i = 0; i < n; i++)
-		prod *= (int)s[i];
-	return (prod ^ 0x55) & 0x3f;
-}
-
-static int f4(const char *s, int n)
-{
-	int max = (int)s[0], i;
-
-	for (i = 0; i < n; i++)
-		if ((int)s[i] > max)
-			max = (int)s[i];
-	srand(max ^ 0xe);
-	return rand() & 0x3f;
-}
-
-static int f5(const char *s, int n)
-{
-	int sum = 0, i;
-
-	for (i = 0; i < n; i++)
-		sum += (int)s[i] * (int)s[i];
-	return (sum ^ 0xef) & 0x3f;
-}
-
-static int f6(int first_char)
-{
-	int r = 0, i;
-
-	for (i = 0; i < first_char; i++)
+	{
+		if ((int)username[i] > max_c)
+			max_c = (int)username[i];
+		sq_sum += (int)username[i] * (int)username[i];
+	}
+	srand(max_c ^ 0xe);
+	idx[0] = rand() & 0x3f;
+	idx[1] = (sq_sum ^ 0xef) & 0x3f;
+	r = 0;
+	for (i = 0; i < (int)username[0]; i++)
 		r = rand();
-	return (r ^ 0xe5) & 0x3f;
+	idx[2] = (r ^ 0xe5) & 0x3f;
 }
 
+/**
+ * main - entry point, prints valid key for given username
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char **argv)
 {
 	char key[7];
-	const char *username;
-	int n;
+	int a[3], b[3], n;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s username\n", argv[0]);
 		return (1);
 	}
-	username = argv[1];
-	n = strlen(username);
-
-	key[0] = charset[f1(n)];
-	key[1] = charset[f2(username, n)];
-	key[2] = charset[f3(username, n)];
-	key[3] = charset[f4(username, n)];
-	key[4] = charset[f5(username, n)];
-	key[5] = charset[f6((int)username[0])];
+	n = strlen(argv[1]);
+	get_indices(argv[1], n, a);
+	get_rand_indices(argv[1], n, b);
+	key[0] = charset[a[0]];
+	key[1] = charset[a[1]];
+	key[2] = charset[a[2]];
+	key[3] = charset[b[0]];
+	key[4] = charset[b[1]];
+	key[5] = charset[b[2]];
 	key[6] = '\0';
-
 	printf("%s\n", key);
 	return (0);
 }
