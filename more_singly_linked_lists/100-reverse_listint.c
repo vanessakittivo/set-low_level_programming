ecb78b65a6e88ecf45365c5be75785af7c62b629
vanessakittivo <wambuinessie@gmail.com>
2026-05-30 17:51:49 +0300

    Add more singly linked lists advanced tasks

--- more_singly_linked_lists/100-reverse_listint.c
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -2,24 +2,28 @@
 
 /**
  * reverse_listint - reverses a listint_t linked list
- * @head: double pointer to the head
+ * @head: double pointer to the head node
  *
  * Return: pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
+	listint_t *prev;
+	listint_t *current;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	prev = NULL;
+	current = *head;
 
 	while (current != NULL)
 	{
-		next = current->next;
+		*head = current->next;
 		current->next = prev;
 		prev = current;
-		current = next;
+		current = *head;
 	}
-
 	*head = prev;
 	return (*head);
 }
