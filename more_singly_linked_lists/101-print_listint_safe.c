f1bebc1b17408ad5349764dc80deff859e10e337
vanessakittivo <wambuinessie@gmail.com>
2026-05-30 18:05:19 +0300

    Add more singly linked lists advanced tasks

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -3,37 +3,54 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list (safely handles loops)
- * @head: pointer to the head
+ * print_listint_safe - prints a listint_t linked list (handles loops)
+ * @head: pointer to the head node
  *
  * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head;
-	const listint_t *fast = head;
-	size_t count = 0;
+	const listint_t *slow;
+	const listint_t *fast;
+	const listint_t *loop_start;
+	size_t count;
 
 	if (head == NULL)
 		return (0);
 
-	while (slow != NULL)
+	slow = head;
+	fast = head;
+	loop_start = NULL;
+
+	while (fast != NULL && fast->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		count++;
-
-		if (fast && fast->next)
-			fast = fast->next->next;
-		else
-			fast = NULL;
-
 		slow = slow->next;
-
-		if (slow == fast && slow != NULL)
+		fast = fast->next->next;
+		if (slow == fast)
 		{
-		printf("-> [%p] %d\n", (void *)slow, slow->n);
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			loop_start = slow;
 			break;
 		}
+	}
+
+	count = 0;
+	while (head != NULL)
+	{
+		if (head == loop_start)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			count++;
+			exit(98);
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		head = head->next;
 	}
 	return (count);
 }
--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -3,29 +3,58 @@
 
 /**
  * free_listint_safe - frees a listint_t list (safely handles loops)
- * @h: double pointer to the head
+ * @h: double pointer to the head node
  *
- * Return: the size of the list that was free'd
+ * Return: the size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *temp;
-	size_t count = 0;
+	listint_t *slow;
+	listint_t *fast;
+	listint_t *loop_start;
+	listint_t *current;
+	listint_t *temp;
+	size_t count;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
+	slow = *h;
+	fast = *h;
+	loop_start = NULL;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = *h;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			loop_start = slow;
+			break;
+		}
+	}
+
+	count = 0;
 	current = *h;
 
 	while (current != NULL)
 	{
-		count++;
+		if (current == loop_start)
+		{
+			free(current);
+			count++;
+			break;
+		}
 		temp = current;
 		current = current->next;
 		free(temp);
-
-		if (current == *h)
-			break;
+		count++;
 	}
 
 	*h = NULL;
